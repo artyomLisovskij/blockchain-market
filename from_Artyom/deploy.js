ca917dfe43b8c74e8676d618a04c9af6fee68d5b
Artyom Lisovskii <a.lisovsky@arriva.ru>
2017-12-15 21:21:34 +0000

    skeleton

---
+++ from_Artyom/deploy.js
@@ -0,0 +1,53 @@
+myContract.deploy({
+    data: '0x12345...',
+    arguments: [123, 'My String']
+})
+.send({
+    from: '0x1234567890123456789012345678901234567891',
+    gas: 1500000,
+    gasPrice: '30000000000000'
+}, function(error, transactionHash){ ... })
+.on('error', function(error){ ... })
+.on('transactionHash', function(transactionHash){ ... })
+.on('receipt', function(receipt){
+   console.log(receipt.contractAddress) // contains the new contract address
+})
+.on('confirmation', function(confirmationNumber, receipt){ ... })
+.then(function(newContractInstance){
+    console.log(newContractInstance.options.address) // instance with the new contract address
+});
+
+
+// When the data is already set as an option to the contract itself
+myContract.options.data = '0x12345...';
+
+myContract.deploy({
+    arguments: [123, 'My String']
+})
+.send({
+    from: '0x1234567890123456789012345678901234567891',
+    gas: 1500000,
+    gasPrice: '30000000000000'
+})
+.then(function(newContractInstance){
+    console.log(newContractInstance.options.address) // instance with the new contract address
+});
+
+
+// Simply encoding
+myContract.deploy({
+    data: '0x12345...',
+    arguments: [123, 'My String']
+})
+.encodeABI();
+> '0x12345...0000012345678765432'
+
+
+// Gas estimation
+myContract.deploy({
+    data: '0x12345...',
+    arguments: [123, 'My String']
+})
+.estimateGas(function(err, gas){
+    console.log(gas);
+});
