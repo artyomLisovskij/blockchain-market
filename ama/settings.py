e3aa77b60cf5737c97e70bbd0d5a2d16e2a44686
Artyom Lisovskii <a.lisovsky@arriva.ru>
2017-12-15 22:17:50 +0000

    new version

--- ama/settings.py
+++ ama/settings.py
@@ -121,6 +121,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
--- ama/urls.py
+++ ama/urls.py
@@ -18,9 +18,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
 from main import urls as main_urls
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(main_urls))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
